687aea888ff736d7febe483fc5a790f7e5f0204c
LethaboM1 <developer@hillary.co.za>
2025-12-17 10:12:29 +0200

    dashboards done

--- FMS/obj/Release/net8.0/FMS.AssemblyInfo.cs
+++ FMS/obj/Release/net8.0/FMS.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FMS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaa26cfb96d63ef51cb379d5f9ecaa076524817")]
 [assembly: System.Reflection.AssemblyProductAttribute("FMS")]
